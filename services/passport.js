a025d20c20bb944a0736137cc8baed2b94ae9636
ngo lam <ngolam>
2018-02-23 02:08:00 +0700

    for to

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
 
 passport.use(new GoogleStratery({
   clientID: keys.googleClientID,
@@ -12,5 +22,16 @@ passport.use(new GoogleStratery({
   callbackURL: '/auth/google/callback'
 }, 
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
 }));
