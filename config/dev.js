f495d387ac08d64c24aeaf8351850146e312b6c7
Ngo Lam <lngo9@csc.com>
2018-02-23 09:50:36 +0700

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js - dont' commit this!!!
+module.exports = {
+  googleClientID: '280982273215-9clp6qf28keumqocin4b7gjgfdkscrpp.apps.googleusercontent.com',
+  googleClientSecret: 'LgVeRsKWOT0NMl3-s_zDVr6t',
+  mongoUri: 'mongodb://lam:password@ds239638.mlab.com:39638/emaily-dev-ngolam',
+  cookieKey: 'idfdjlfkjjjfkjksldfjfkjsldjfklsdkjflskdjfl'
+}
