ddd0656b81207b42455a12c8c0f2f6742aaeb445
Ngo Lam <lngo9@csc.com>
2018-02-27 17:24:44 +0700

    78

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@
 module.exports = {
   googleClientID: '280982273215-9clp6qf28keumqocin4b7gjgfdkscrpp.apps.googleusercontent.com',
   googleClientSecret: 'LgVeRsKWOT0NMl3-s_zDVr6t',
-  mongoUri: 'mongodb://lam:password@ds239638.mlab.com:39638/emaily-dev-ngolam',
+  mongoUri: 'mongodb://lam:lam@ds249798.mlab.com:49798/emaily-dev-ngolam',
   cookieKey: 'idfdjlfkjjjfkjksldfjfkjsldjfklsdkjflskdjfl'
 }
