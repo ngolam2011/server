269246352a6fa0815d550be2f5fbc38dd46faed9
ngo lam <ngolam>
2018-02-28 07:42:42 +0700

    89

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import * as actions from  '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
   
 
@@ -21,8 +21,8 @@ class App extends Component {
           <div>
             <Header />
             <Route exact path="/" component={Landing} />
-            <Route exact path="/serveys" component={Dashboard} />
-            <Route path="/serveys/new" component={SurveyNew} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
