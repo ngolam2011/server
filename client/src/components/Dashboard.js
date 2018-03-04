438771c3aaf5a760d6adb669c2e8d7e768337015
ngo lam <ngolam>
2018-03-04 18:33:44 +0700

    final

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SurveyList from './surveys/SurveyList';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <SurveyList />
       <div className="fixed-action-btn">
        <Link to="/surveys/new" href="" className="btn-floating btn-large red">
         <i className="material-icons">add</i>
--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@
 module.exports = {
   googleClientID: '280982273215-9clp6qf28keumqocin4b7gjgfdkscrpp.apps.googleusercontent.com',
   googleClientSecret: 'LgVeRsKWOT0NMl3-s_zDVr6t',
-  mongoUri: 'mongodb://localhost:27017/emaily-dev',
+  mongoUri: 'mongodb://lam:lam@ds153958.mlab.com:53958/emaily-lam',
   cookieKey: 'idfdjlfkjjjfkjksldfjfkjsldjfklsdkjflskdjfl',
   stripePublishableKey: 'pk_test_iBtb8rCf8ZJruBBHrGw5C7rA',
   stripeSecretKey: 'sk_test_dwkFYUeRjvZEbrIG7ovLET9H',
